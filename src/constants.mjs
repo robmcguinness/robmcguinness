b6dd51910e6834fe4c46cc3b5d39259312074c86
Robert McGuinness <robert.mcguinness.iii@gmail.com>
2025-02-14 22:11:06 -0500

    feat: astro

---
+++ src/constants.mjs
@@ -0,0 +1,15 @@
+export const SITE_URL = "https://robmcguinness.com";
+export const SITE_TITLE = "Rob McGuinness Blog";
+export const SITE_DESCRIPTION = "Personal website and blog for Rob McGuinness";
+export const SITE_AUTHOR = "Rob McGuinness";
+export const SITE_KEYWORDS = "Rob McGuinness, software engineer, engineering leader, blog, personal website";
+export const SITE_SOCIAL_HANDLE = "@robmcguinness";
+export const PAGINATION_INT = 50;
+
+export const NAVIGATION_LINKS = [
+  { name: "Home", href: "/" },
+  { name: "Blog", href: "/blog" },
+  { name: "Bluesky", href: "https://bsky.app/profile/robmcguinness.com" },
+  { name: "GitHub", href: "https://www.github.com/robmcguinness" },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/robmcguinness/" },
+];
